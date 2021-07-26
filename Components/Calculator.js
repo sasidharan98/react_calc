32db09a3c661f519941adef11b8d04826df34ce6
sasidharan98 <sasi2013dharan@gmail.com>
2021-07-26 17:00:06 +0530

    Added decimal functionality and useEffect hook

--- Components/Calculator.js
+++ Components/Calculator.js
@@ -1,24 +1,53 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import '../Stylesheets/Calculator.css';
 import '../src/style.css';
 const Calculator = () => {
   let [currElement, setcurrElement] = useState('');
   let [prevElement, setprevElement] = useState('');
+  let [operator, setOperator] = useState('');
+  let [decimal, setdecimalOperator] = useState('');
   const Handler = event => {
     const values = event.target;
-    console.log(values.className);
-    if (values.className === 'number') {
-      setcurrElement((currElement += values.innerHTML.toString()));
+    if (values.className === 'number' && values.innerHTML !== '.') {
+      setcurrElement(() => {
+        currElement += values.innerHTML.toString();
+        currElement = parseFloat(currElement);
+        return (currElement = currElement.toString());
+      });
+      console.log(currElement);
     }
     if (values.className === 'operand') {
-      setprevElement((prevElement += currElement));
-      chooseOperation(values.innerHTML);
+      setOperator(() => {
+        if (operator !== '') {
+          return operator;
+        } else {
+          return (operator = values.innerHTML);
+        }
+      });
+      chooseOperation(operator);
+      setprevElement((prevElement = currElement));
+      setcurrElement((currElement = ''));
+      setOperator((operator = values.innerHTML));
+    }
+    if (values.innerHTML === '.') {
+      setcurrElement(() => {
+        if (currElement.includes('.')) {
+          console.log('1');
+          return currElement;
+        } else {
+          console.log(currElement.slice(-1));
+          return (currElement += values.innerHTML);
+        }
+      });
     }
     if (values.innerHTML === '=') {
-      compute(values);
+      compute(operator);
     }
     if (values.innerHTML === 'AC') {
       clear();
+    }
+    if (values.innerHTML === 'DEL') {
+      setcurrElement((currElement = currElement.toString().slice(0, -1)));
     }
   };
   const chooseOperation = operand => {
@@ -31,24 +60,49 @@ const Calculator = () => {
     let computation;
     const prevValue = parseFloat(prevElement);
     const currValue = parseFloat(currElement);
+    console.log(prevValue, currValue, operand);
     if (isNaN(prevValue) || isNaN(currValue)) return;
     switch (operand) {
       case '*':
         computation = prevValue * currValue;
         break;
+      case '-':
+        computation = prevValue - currValue;
+        break;
+      case '+':
+        computation = prevValue + currValue;
+        break;
+      case '/':
+        computation = prevValue / currValue;
+        break;
+      default:
+        return;
     }
     setcurrElement((currElement = computation));
+    setprevElement((prevElement = ''));
+    setOperator((operator = ''));
   };
   const clear = () => {
     setprevElement((prevElement = ''));
     setcurrElement((currElement = ''));
+    setOperator((operator = ''));
   };
+  useEffect(() => {
+    setdecimalOperator(
+      (decimal = currElement.toLocaleString('en', {
+        maximumFractionDigits: 0
+      }))
+    );
+    console.log('effect');
+  }, [currElement]);
+
   return (
     <>
       <div className="calculator-body">
         <div className="output">
           <div className="previousOperationText">{prevElement}</div>
-          <div className="currentOperationText">{currElement}</div>
+          <div className="operand">{operator}</div>
+          <div className="currentOperationText">{decimal}</div>
         </div>
         <div className="operators" onClick={Handler}>
           <button className="allClear span-two">AC</button>
