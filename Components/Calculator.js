db46daeab890b6d9fa565a2f026fb18a8d576102
sasidharan98 <sasi2013dharan@gmail.com>
2021-07-26 12:50:08 +0530

    2 files modified

--- Components/Calculator.js
+++ Components/Calculator.js
@@ -1,16 +1,56 @@
-import React from 'react';
+import React, { useState } from 'react';
 import '../Stylesheets/Calculator.css';
 import '../src/style.css';
 const Calculator = () => {
-  
+  let [currElement, setcurrElement] = useState('');
+  let [prevElement, setprevElement] = useState('');
+  const Handler = event => {
+    const values = event.target;
+    console.log(values.className);
+    if (values.className === 'number') {
+      setcurrElement((currElement += values.innerHTML.toString()));
+    }
+    if (values.className === 'operand') {
+      setprevElement((prevElement += currElement));
+      chooseOperation(values.innerHTML);
+    }
+    if (values.innerHTML === '=') {
+      compute(values);
+    }
+    if (values.innerHTML === 'AC') {
+      clear();
+    }
+  };
+  const chooseOperation = operand => {
+    if (currElement === '') return;
+    if (prevElement != '') {
+      compute(operand);
+    }
+  };
+  const compute = operand => {
+    let computation;
+    const prevValue = parseFloat(prevElement);
+    const currValue = parseFloat(currElement);
+    if (isNaN(prevValue) || isNaN(currValue)) return;
+    switch (operand) {
+      case '*':
+        computation = prevValue * currValue;
+        break;
+    }
+    setcurrElement((currElement = computation));
+  };
+  const clear = () => {
+    setprevElement((prevElement = ''));
+    setcurrElement((currElement = ''));
+  };
   return (
     <>
       <div className="calculator-body">
         <div className="output">
-          <div className="previousOperationText" >123</div>
-          <div className="currentOperationText" >456</div>
+          <div className="previousOperationText">{prevElement}</div>
+          <div className="currentOperationText">{currElement}</div>
         </div>
-        <div className="operators">
+        <div className="operators" onClick={Handler}>
           <button className="allClear span-two">AC</button>
           <button className="delete">DEL</button>
           <button className="operand">/</button>
